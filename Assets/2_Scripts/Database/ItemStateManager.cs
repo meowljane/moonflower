3f33e72029e562d7f15d5a2d7baececbf3b7a2a8
meowljane <dhy04144@gmail.com>
2024-11-05 15:10:11 +0900

    업데이트

--- Assets/2_Scripts/Database/ItemStateManager.cs
+++ Assets/2_Scripts/Database/ItemStateManager.cs
@@ -1,13 +1,9 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemStateManager : MonoBehaviour
-{
-    public string itemName;
-    public Sprite inactiveSprite;
-    public Sprite activeSprite; 
-
-    public GameObject usedObj;   
+{  
 
     private void OnEnable()
     {
@@ -17,13 +13,16 @@ public class ItemStateManager : MonoBehaviour
     private void UpdateButtonState()
     {
         DatabaseManager databaseManager = DatabaseManager.instance;
-        foreach (ItemInfo itemInfo in DatabaseManager.instance.itemInfos)
-        {
-            ItemInfo.ItemData? itemData = itemInfo.items.Find(q => q.itemName == itemName);
 
+        foreach (ItemInfo itemInfo in databaseManager.itemInfos)
+        {
             if (itemInfo != null)
             {
+                // Count items with status == true
+                int trueStatusCount = itemInfo.items.Count(item => item.status);
 
+                // Log the room name and the count of items with status == true
+                Debug.Log($"Room: {itemInfo.roomName} has {trueStatusCount} item(s) with status set to true.");
             }
         }
     }
