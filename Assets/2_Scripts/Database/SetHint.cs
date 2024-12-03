c2dbe0f96eed4eb77626bb973cd82c685d516d0f
meowljane <dhy04144@gmail.com>
2024-12-04 05:07:45 +0900

    door

--- Assets/2_Scripts/Database/SetHint.cs
+++ Assets/2_Scripts/Database/SetHint.cs
@@ -7,14 +7,7 @@ public class SetHint : MonoBehaviour
 
     private void Awake()
     {
-        theDM = FindObjectOfType<DatabaseManager>();
-    }
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            theDM.isCheckedHint = true;
-            ceiling.SetActive(false);
-        }
+        theDM = FindFirstObjectByType<DatabaseManager>();
+        theDM.isCheckedHint = true;
     }
 }
--- Assets/2_Scripts/Object/InteractionDoor.cs
+++ Assets/2_Scripts/Object/InteractionDoor.cs
@@ -2,10 +2,17 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class InteractionPanel : MonoBehaviour
-{    
+public class InteractionDoor : MonoBehaviour
+{
     //스크립트 캐싱
     public PanelManager thePM;
+
+    //활성화, 비활성화할 오브젝트들
+    public GameObject[] objectsToDisable;
+    public GameObject[] objectsToEnable;
+
+    // Sprite 배열 미리 받아두는곳
+    public List<Sprite> spriteData;
 
     //오브젝트 캐싱
     public GameObject confirmOn;
@@ -15,8 +22,6 @@ public class InteractionPanel : MonoBehaviour
     public bool isColliding = false;
     public bool isTouch = false;
 
-    // Sprite 배열 미리 받아두는곳
-    public List<Sprite> spriteData;
 
     private void Awake()
     {
@@ -34,20 +39,21 @@ public class InteractionPanel : MonoBehaviour
             {
                 if (spriteData != null && spriteData.Count > 0)
                 {
-                    thePM.ShowPanel(spriteData, false);
+                    thePM.ShowPanel(spriteData, true);
+                }
+                else
+                {
+                    UpdateObjects();
                 }
                 webglBtn.isClick = false;
             }
-            if (isTouch)
-            {
-                thePM.ShowPanel(spriteData, false);
-            }
-
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         isColliding = true;
+        thePM.objectsToDisable = objectsToDisable;
+        thePM.objectsToEnable = objectsToEnable;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -59,5 +65,17 @@ public class InteractionPanel : MonoBehaviour
     public void ClickPanelEvent()
     {
         thePM.ShowPanel(spriteData, false);
+    }
+
+    private void UpdateObjects()
+    {
+        foreach (GameObject obj in objectsToDisable)
+        {
+            obj.SetActive(false);
+        }
+        foreach (GameObject obj in objectsToEnable)
+        {
+            obj.SetActive(true);
+        }
     }
 }
