bb726430fc65c8dab7901ea0d94cca714244a2a3
meowljane <dhy04144@gmail.com>
2024-12-04 16:01:06 +0900

    F7 씬 초기화 기능

--- Assets/2_Scripts/Database/SetHint.cs
+++ Assets/2_Scripts/Database/SetHint.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class SetHint : MonoBehaviour
 {
     private DatabaseManager theDM;
-    public GameObject ceiling;
+    public GameObject[] objectsToDisable;
 
     private void Awake()
     {
         theDM = FindFirstObjectByType<DatabaseManager>();
         theDM.isCheckedHint = true;
+        UpdateObjects();
+    }
+
+    private void UpdateObjects()
+    {
+        foreach (GameObject obj in objectsToDisable)
+        {
+            obj.SetActive(false);
+        }
     }
 }
